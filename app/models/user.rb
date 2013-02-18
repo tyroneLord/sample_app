97746681dbc8e668e5e220a1573dd6036fb76284
Tyrone Gaylord <tyrone.mdd@gmail.com>
2013-02-18 13:10:30 -0600

    added encrytiion to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
-  validates :name, presence: true
-  validates :email, presence: true
+  before_save { |user| user.email = user.email.downcase}
+
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i #THIS MAKING A CONSTANT VAR WITH REGEX
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
 end
