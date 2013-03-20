7a5a09769c5b4459da5b0f25300ef5d0577530db
Tyrone Gaylord <tyrone.mdd@gmail.com>
2013-03-19 22:35:20 -0500

    making sign in and sign out bttn

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,19 @@
 module SessionsHelper
 	def sign_in(user)
 		#sign the user in
+		cookies.permanent[:remember_token] = user.remember_token
+		current_user = user
+	end
 
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		@current_user ||= User.find_by_remember_token(cookies[:remeber_token])
 	end
 end
